e7b8994eb0281e3e9a8848bf7f353d35d11bd776
Sarvjeet Swanshi <adityaspgm@gmail.com>
2024-12-26 19:37:26 +0530

    new1

--- socket/index.js
+++ socket/index.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server(9000, {
     cors: {
-        origin: 'https://chatify-3gfo.onrender.com'
+        origin: 'https://chatify-gqit.onrender.com'
     }
 });
 
